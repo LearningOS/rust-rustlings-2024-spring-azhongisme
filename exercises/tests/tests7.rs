bcf6a8fda889c2937017414fda9ff18152628a4d
azhongisme <1322501114@qq.com>
2024-04-28 13:00:21 +0800

    100

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -32,9 +32,30 @@
 // to pass this exercise.
 //
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
+// hint.// - Building a bundled C library.
+// - Finding a C library on the host system.
+// - Generating a Rust module from a specification.
+// - Performing any platform-specific configuration needed for the crate.
+//
+// When setting up configurations, we can `println!` in the build script
+// to tell Cargo to follow some instructions. The generic format is:
+//
+//     println!("cargo:{}", your_command_in_string);
+//
+// Please see the official Cargo book about build scripts for more
+// information:
+// https://doc.rust-lang.org/cargo/reference/build-scripts.html
+//
+// In this exercise, we look for an environment variable and expect it to
+// fall in a range. You can look into the testcase to find out the details.
+//
+// You should NOT modify this file. Modify `build.rs` in the same directory
+// to pass this exercise.
+//
+// Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {}
 
